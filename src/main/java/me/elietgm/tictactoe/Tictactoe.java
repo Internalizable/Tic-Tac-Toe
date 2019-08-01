918e21d7026f3b9d192754e9c4c16af1a226e8c5
Internalizable <elietgm1@gmail.com>
2019-08-01 18:40:11 +0300

    First commit

---
+++ src/main/java/me/elietgm/tictactoe/Tictactoe.java
@@ -0,0 +1,17 @@
+package me.elietgm.tictactoe;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Tictactoe extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
